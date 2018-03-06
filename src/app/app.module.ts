2bf354a576a4946c57da4610d819ac349bfa7194
Hamza Hamidi <hamza.hamidi@orange.com>
2018-03-06 16:46:51 +0100

    Add sample editing service paramaters
    Signed-off-by: Hamza Hamidi <hamza.hamidi@orange.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import { AppComponent, AppServiceParamsComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CdkTableModule } from '@angular/cdk/table';
@@ -47,8 +47,10 @@ import {
 @NgModule({
   declarations: [
     AppComponent,
+    AppServiceParamsComponent
 
   ],
+  entryComponents: [AppServiceParamsComponent],
   imports: [
     MaterialDesignFrameworkModule,
     JsonSchemaFormModule.forRoot(MaterialDesignFrameworkModule),
