0871cfdf647574799bfde94e419fde895f632af1
Hamza HAMIDI <hamidihamzacg@gmail.com>
2020-01-12 19:00:31 +0100

    Feat: INIT PROJECT TO USE AJSF CORE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,65 +1,12 @@
-import { Component, Inject, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-declare let JSONEditor;
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  constructor(public dialog: MatDialog) { }
-
-  openDialogServiceParams(): void {
-    const dialogRef = this.dialog.open(AppServiceParamsComponent, {
-      height: '600px',
-      width: '600px'
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-    });
-  }
-}
-
-@Component({
-  selector: 'app-service-params-component',
-  template: `
-  <div>
-  <mat-grid-list cols="2" rowHeight="5:1">
-  <mat-grid-tile></mat-grid-tile>
-  <mat-grid-tile>
-  <button style="position: fixed ;z-index: 9999 ;" mat-raised-button color="accent" 
-  (click)="generatedFormOrJsonEditor=!generatedFormOrJsonEditor"> 
-  <span *ngIf="generatedFormOrJsonEditor">Switch to Json Editor</span>
-  <span *ngIf="!generatedFormOrJsonEditor">Switch to Generated Forms</span>
-  </button>
-  </mat-grid-tile>
-  </mat-grid-list> 
-  </div>
-  <div [hidden]="generatedFormOrJsonEditor">
-  <div #jsoneditor style="height: 400px;"></div>
-  <button style="margin:5px;" mat-raised-button color="accent" 
-  (click)="done()"> Done
-  </button>
-  </div> 
-  <div [hidden]="!generatedFormOrJsonEditor">
-  <mat-card class="card">
-  <json-schema-form
-  loadExternalAssets="true"
-  [schema]="schema"
-  [framework]="selectedFramework"
-  (onSubmit)="submit($event)">
-  </json-schema-form>
-  </mat-card>
-  </div>`
-})
-export class AppServiceParamsComponent implements AfterViewInit {
-  generatedFormOrJsonEditor: boolean;
-  selectedFramework = 'material-design';
-  @ViewChild('jsoneditor') container: ElementRef;
-  editor;
+  selectedFramework = 'no-framwork';
   schema = {
     'type': 'object',
     'title': 'test',
@@ -145,34 +92,4 @@ export class AppServiceParamsComponent implements AfterViewInit {
 
     }
   };
-
-  constructor(
-    public dialogRef: MatDialogRef<AppServiceParamsComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) { }
-
-  ngAfterViewInit() {
-    console.log(this.container);
-    console.log(this.container.nativeElement);
-    const container = document.getElementById('jsoneditor');
-    const options = {
-      mode: 'code'
-    };
-    this.editor = new JSONEditor(this.container.nativeElement, options);
-    const json = {};
-    this.editor.set(json);
-
-  }
-
-  submit($event) {
-    console.log($event);
-    alert(JSON.stringify($event, null, 2));
-    this.dialogRef.close();
-  }
-
-  done() {
-    const output = this.editor.get();
-    alert(JSON.stringify(output, null, 2));
-    console.log(output);
-    this.dialogRef.close();
-  }
 }
