853c1891c907b3f1e2bda3c61e8c8f970cfd5fb4
Hamza HAMIDI <hamidihamzacg@gmail.com>
2020-01-12 19:00:31 +0100

    Feat: INIT PROJECT TO USE AJSF CORE

--- README.md
+++ README.md
@@ -1,27 +1,25 @@
+# ajsf demo
 
-[![Build Status](https://travis-ci.org/hamzahamidi/schema-form-generator.svg?branch=develop)](https://travis-ci.org/hamzahamidi/schema-form-generator)
-# Json schema form generator Sample with Angular 
+Demo application using [Angular JSON Schema Form application core](https://github.com/hamzahamidi/ajsf)
 
-
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.6. It includes [Angular2-json-schema-form](https://github.com/dschnelldavis/angular2-json-schema-form).
-The DEMO is available [here](https://hamidihamza.com/schema-form-generator/).
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.22.
 
 ## Development server
 
-Using NPM
-
-```
-$ npm install
-```
-Using YARN
-```
-$ yarn install
-```
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
 
 ## Build
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `-prod` flag for a production build.
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+
+## Running unit tests
+
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Running end-to-end tests
+
+Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
 
 ## Further help
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,93 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Bootstrap3FrameworkModule } from '@ajsf/bootstrap3';
 
-
-import { AppComponent, AppServiceParamsComponent } from './app.component';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CdkTableModule } from '@angular/cdk/table';
-
-import {
-  MatAutocompleteModule,
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatExpansionModule,
-  MatGridListModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatNativeDateModule,
-  MatPaginatorModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatRippleModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSliderModule,
-  MatSlideToggleModule,
-  MatSnackBarModule,
-  MatSortModule,
-  MatTableModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
-  MatStepperModule,
-} from '@angular/material';
-import {
-  JsonSchemaFormModule, MaterialDesignFrameworkModule
-} from 'angular2-json-schema-form';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AppServiceParamsComponent
-
+    AppComponent
   ],
-  entryComponents: [AppServiceParamsComponent],
   imports: [
-    MaterialDesignFrameworkModule,
-    JsonSchemaFormModule.forRoot(MaterialDesignFrameworkModule),
     BrowserModule,
-    BrowserAnimationsModule,
-    CdkTableModule,
-    MatAutocompleteModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatChipsModule,
-    MatStepperModule,
-    MatDatepickerModule,
-    MatDialogModule,
-    MatExpansionModule,
-    MatGridListModule,
-    MatIconModule,
-    MatInputModule,
-    MatListModule,
-    MatMenuModule,
-    MatNativeDateModule,
-    MatPaginatorModule,
-    MatProgressBarModule,
-    MatProgressSpinnerModule,
-    MatRadioModule,
-    MatRippleModule,
-    MatSelectModule,
-    MatSidenavModule,
-    MatSliderModule,
-    MatSlideToggleModule,
-    MatSnackBarModule,
-    MatSortModule,
-    MatTableModule,
-    MatTabsModule,
-    MatToolbarModule,
-    MatTooltipModule,
+    Bootstrap3FrameworkModule
   ],
   providers: [],
   bootstrap: [AppComponent]
